ff931c19697033d07ec20f23396d40386c56413e
hmoon630 <skyuecx0630@gmail.com>
2024-03-05 19:52:32 +0900

    cdk-nag

--- gummy-promotion/bin/gummy-promotion.ts
+++ gummy-promotion/bin/gummy-promotion.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
+import { AwsSolutionsChecks } from "cdk-nag";
 import { GummyPromotionStack } from "../lib/gummy-promotion-stack";
 
 const devEnv = {
@@ -9,6 +10,10 @@ const devEnv = {
 };
 
 const app = new cdk.App();
+
+cdk.Aspects.of(app).add(new AwsSolutionsChecks());
+// cdk.Aspects.of(app).add(new NIST80053R5Checks());
+
 new GummyPromotionStack(app, "GummyPromotionStack", {
   env: devEnv,
 });
