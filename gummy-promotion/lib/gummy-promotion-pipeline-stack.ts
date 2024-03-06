07c555ae31358826f467eec2cff56507d5b634d4
hmoon630 <skyuecx0630@gmail.com>
2024-03-06 16:43:00 +0900

    Initialize codecommit repository itself

--- gummy-promotion/lib/gummy-promotion-pipeline-stack.ts
+++ gummy-promotion/lib/gummy-promotion-pipeline-stack.ts
@@ -1,6 +1,6 @@
-import { Stack, StackProps, Stage } from "aws-cdk-lib";
+import { IgnoreMode, Stack, StackProps, Stage } from "aws-cdk-lib";
 import { BuildSpec } from "aws-cdk-lib/aws-codebuild";
-import { Repository } from "aws-cdk-lib/aws-codecommit";
+import { Code, Repository } from "aws-cdk-lib/aws-codecommit";
 import {
   CodePipeline,
   CodePipelineSource,
@@ -8,6 +8,8 @@ import {
 } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 import { GummyPromotionStack } from "./gummy-promotion-stack";
+import { Asset } from "aws-cdk-lib/aws-s3-assets";
+import * as fs from "fs";
 
 class GummyPromotionStage extends Stage {
   constructor(scope: Construct, id: string) {
@@ -21,9 +23,26 @@ export class GummyPromotionPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    let gitignore = fs.readFileSync(".gitignore").toString().split(/\r?\n/);
+    gitignore.push(".git/");
+
+    // Allow canary code to package properly
+    // see: https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch_Synthetics_Canaries_WritingCanary_Nodejs.html#CloudWatch_Synthetics_Canaries_package
+    gitignore = gitignore.filter((g: string) => g != "node_modules/");
+    gitignore.push("/node_modules/");
+
+    const codeAsset = new Asset(this, "SourceAsset", {
+      path: ".",
+      ignoreMode: IgnoreMode.GIT,
+      exclude: gitignore,
+    });
+
     const source = CodePipelineSource.codeCommit(
-      Repository.fromRepositoryName(this, "Source", "gummy-promotion"),
-      "test"
+      new Repository(this, "Source", {
+        repositoryName: "gummy-promotion",
+        code: Code.fromAsset(codeAsset, "main"),
+      }),
+      "main"
     );
 
     const pipeline = new CodePipeline(this, "GummyPromotionPipeline", {
